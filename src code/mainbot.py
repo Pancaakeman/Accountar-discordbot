1fffaa0e0cea3d7756dc36b4112ab2b135be7e21
Haxstarcoder <haxionrising@gmail.com>
2024-10-31 19:28:01 +0000

    added some subprograms

--- src code/mainbot.py
+++ src code/mainbot.py
@@ -1,5 +1,5 @@
 import discord
-from discord.ext import commands
+from discord.ext import commands,tasks
 from discord import app_commands
 from config import Configs
 from itertools import cycle
@@ -7,20 +7,35 @@ from itertools import cycle
 c = Configs
 
 
-token = ""
-statuses = ["Gready atm","I am the sin of Greed","🤑💶💸💳","Slide me the money","Dead from your Poison"]
+token = "MTI5MjkyNzAxMDc5NjIwODIzMA.G5Aqt1.ecmiT82DPzPlhxpkoCqkjHWknoLgoVj4c3AOBQ"
 
+statuses = ["Gready atm","I am the sin of Greed","🤑💶💸💳","Slide me the money","Dead from your Poison","Sponsored by I.M.P"]
+
+status = cycle(statuses)
 
 intents = discord.Intents.all()
 client = discord.Client(intents=intents)
 tree = app_commands.CommandTree(client)
 
+@tasks.loop(seconds=30)
+async def change_status(): 
+    await client.change_presence(status=discord.Status.online)
+    await client.change_presence(activity=discord.CustomActivity(name=next(status)))
 
-
-@client.event
-async def on_ready():
-    activity = discord.Game(name=cycle(statuses))
-    await client.change_presence(status=discord.Status.online, activity=activity)
+@client.event 
+async def on_ready(): 
+    change_status.start() 
     await tree.sync()
-    print("Ready!")
+    await print(f'Logged in as {client.user}!') 
     
+    
+    
+@tree.command(name="collect",description="collect your salary")
+async def collect(userid,user_roles):
+    return
+    
+
+
+    
+    
+client.run(token)
---
+++ src code/कर्मचारी.py
@@ -0,0 +1,12 @@
+import discord
+
+
+class Karamchari:
+    def __init__(self):
+        pass
+    
+    async def add_salary(userid,role):
+        return
+    async def collect(userid,user_roles):
+        return
+    
