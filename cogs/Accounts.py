3d4fd9dd2bf4edf40e3d7aaa7940bd1d692395ad
Haxstarcoder <haxionrising@gmail.com>
2025-01-05 03:18:50 +0000

    Added cogs will migrate other commands soon

---
+++ cogs/Accounts.py
@@ -0,0 +1,88 @@
+import discord
+from discord import app_commands
+from discord.ext import commands
+from Database_Managers.Assist import Assister
+from Database_Managers.Worker import Worker
+
+
+
+class Accounting(commands.Cog):
+    def __init__(self, bot,a,k):
+        self.bot = bot
+        self.a = a
+        self.k = k
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print(f"{self.__class__.__name__} Cog has been loaded")
+        await self.bot.tree.sync()        
+        
+    @app_commands.command(name="enroll",description="Enables you to create an account")
+    async def enrollment(self,interaction :discord.interactions):
+        try:
+            check1 = await self.k.create_acc(userid=interaction.user.id)
+            check2 = await self.a.enroll_salary(userid=interaction.user.id)
+            
+            if check2 is not None and check1 is not None:
+                await self.a.id_add(userid=interaction.user.id)
+                embed= discord.Embed(title=f"A bank account has been successfully created for: {interaction.user.display_name}",colour=discord.Color.brand_green())
+                embed.add_field(name="Run `/account` to check out your account!")
+                embed.set_footer(text="Welcome aboard!")
+                embed.set_author( name=f"{interaction.user.name}", icon_url=interaction.user.avatar.url )
+                await interaction.response.send_message(embed = embed)
+            
+            else:
+                embed = discord.Embed( title="🚫 Account Creation Limit Reached!", description="You cannot create more than one bank account. Please contact support if you need assistance.", color=discord.Colour.red() ) 
+                embed.set_author( name=f"{interaction.user.name}", icon_url=interaction.user.avatar.url )
+                await interaction.response.send_message(embed = embed)
+        except Exception as e: 
+            print(e)
+            
+    @app_commands.command(name="account",description="Enables you to view your own or other people's bank accounts")
+    async def view(self,interaction :discord.Interaction,user : discord.Member = None):
+        try:
+            if user is None:
+                user = interaction.user.id
+            
+            if await self.a.account_check(interaction,user=user.id) is None:
+                return None
+            else:
+                print(user)
+                    
+                check = await self.k.display_bank(userid=user.id)
+                bank_value = check[1]
+                embed = discord.Embed(
+                    title="🏦 Bank Details",
+                    color=discord.Color.dark_teal()
+                )
+                embed.set_thumbnail(url="https://cdn-icons-png.flaticon.com/128/2830/2830284.png")
+                embed.add_field(
+                    name="💵 Account Number:",
+                    value=f"{user.id}",
+                    inline=True
+                )
+                embed.add_field(
+                    name="💵 Account Holder:",
+                    value=f"{user.mention}",
+                    inline=True
+                )
+                embed.add_field(
+                    name="💵 Balance:",
+                    value=f"£{bank_value}",
+                    inline=False
+                )
+                embed.set_footer(text=f"You are in the {interaction.guild.name} Branch")
+                embed.set_author(
+                    name=f"{interaction.user.name}",
+                    icon_url=interaction.user.avatar.url
+                )
+                
+                await interaction.response.send_message(embed=embed)
+                
+        except Exception as e:
+            print(e)
+        
+
+
+async def setup(bot):
+    await bot.add_cog(Accounting(bot,a = Assister("Databases/Warehouse.db"),k = Worker("Databases/Warehouse.db")))
+    print(f"Economy Cog Loaded")
---
+++ mainbot.py
@@ -0,0 +1,92 @@
+import discord
+import asyncio
+from discord.ext import tasks,commands
+from discord import app_commands
+from itertools import cycle
+from Database_Managers import Assister, Worker
+import datetime
+import random
+import os
+
+#Variables
+
+
+RESET_TIME = datetime.time(hour=0,minute=0,second=0)
+
+
+
+k = Worker("src code/Databases/Warehouse.db")
+a = Assister("src code/Databases/Warehouse.db")
+error = discord.Embed( title="🚨 Internal Error 🚨", description="Something went wrong! Please open a ticket to report the issue and we'll get it resolved as soon as possible.", color=discord.Colour.red())
+
+
+
+with open("token.txt","r") as t:
+    token = t.read()
+
+statuses = ["Gready atm","I am the sin of Greed","🤑💶💸💳","Dead from your Poison","Sponsored by I.M.P","I eat Money for Breakfast"]
+
+status = cycle(statuses)
+
+intents = discord.Intents.all()
+intents.members = True
+bot = commands.Bot(command_prefix="!",intents=intents)
+
+  
+    
+#------------------------------------------
+#SUBPROGRAMS
+#------------------------------------------                
+async def check_collect_reset():
+    while True:
+        current = datetime.datetime.now()
+        reset_time = datetime.datetime(year=current.year, month=current.month, day=current.day, hour=0, minute=0, second=0)
+        
+        if current >= reset_time:
+            reset_time = reset_time + datetime.timedelta(days=1)
+        
+        time_until_reset = (reset_time - current).total_seconds()
+        print(f"Time until reset: {time_until_reset} seconds")
+        await asyncio.sleep(time_until_reset)
+        await a.daily_reset_collect()
+        await asyncio.sleep(86400)
+        
+
+    
+async def load():
+    for filename in os.listdir('./cogs'):  
+        if filename.endswith('.py'): 
+            await bot.load_extension(f'cogs.{filename[:-3]}')
+            
+async def main():
+    async with bot:
+        await load()
+        await bot.start("MTI4MDEyODYxNzMxMjk0NDE0OA.GCQQ9K.wASuc-RvRcKepRwQn7ERgBdtkboaGyz4G5pue4")
+
+        
+        
+#------------------------------------------
+#EVENTS
+#------------------------------------------
+@tasks.loop(seconds=30)
+async def change_status(): 
+    await bot.change_presence(status=discord.Status.online)
+    await bot.change_presence(activity=discord.CustomActivity(name=next(status)))
+    
+
+
+    
+
+@bot.event 
+async def on_ready(): 
+    change_status.start()
+    await check_collect_reset()
+    await bot.tree.sync()
+    await print("Tree Synced")
+    
+    
+asyncio.run(main=main())
+
+
+
+
